6062d712dd0216f2096c95853567dac0c3dbda92
ngarcia <ngarcia@up.edu.ph>
2023-08-08 03:40:17 +0800

    Implement basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, createRoutesFromElements, Route, Link, Outlet, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+    )
+  )
+
   return (
     <>
+      <RouterProvider router={router} />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -31,5 +44,21 @@ function App() {
     </>
   )
 }
+
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+      </div>
+
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
+}
+
 
 export default App
