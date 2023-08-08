420e6422cd0c3006bd24d1719db804e8a3657ac4
ngarcia <ngarcia@up.edu.ph>
2023-08-08 20:35:09 +0800

    Implement login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,29 @@
+// Library Imports
+import { useContext } from "react"
 
+// Component Imports
+import AuthContext from "../context/AuthContext"
+
+// Asset Imports
+
+// Style Imports
 
 function Login() {
+    let { loginUser } = useContext(AuthContext)
+
     return (
-        <body>
-            <div>
-                <form>
-                    <h1>Login</h1>
-                    <div>
-                        <input type="text" name="username" placeholder="Enter username" />
-                    </div>
-                    <div>
-                        <input type="password" name="password" placeholder="Enter password" />
-                    </div>
-                    <button type="submit">Login</button>
-                </form>
-            </div>
-        </body>
+        <div>
+            <form onSubmit={loginUser}>
+                <h1>Login</h1>
+                <div>
+                    <input type="text" name="username" placeholder="Enter username" />
+                </div>
+                <div>
+                    <input type="password" name="password" placeholder="Enter password" />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
     )
 }
 
