85d927a1431843e929434368434cd89e64e0ec3b
ngarcia <ngarcia@up.edu.ph>
2023-08-10 13:32:02 +0800

    Implement react table

--- src/pages/Taxonomy/Domain/DomainTable.jsx
+++ src/pages/Taxonomy/Domain/DomainTable.jsx
@@ -1,26 +1,86 @@
 import axios from "axios";
 import { useState, useEffect } from "react"
+import { createColumnHelper, flexRender, getCoreRowModel, useReactTable, getPaginationRowModel  } from "@tanstack/react-table";
+import data from "./data.json"
+const columnHelper = createColumnHelper()
+const columns = [
+    {
+        accessorKey: 'id',
+        header: 'ID',
+    },
+    {
+        accessorKey: 'category_name',
+        header: 'Name'
+    },
+    {
+        accessorKey: 'scientific_name',
+        header: 'Scientific Name'
+    }
+]
 
 function DomainTable() {
-    const [data, setData] = useState([]);
+    // const data = [
+    //     {
+    //         "id": 0,
+    //         "category_name": "nerd",
+    //         "scientific_name": "nerd?"
+    //     },
+    //     {
+    //         'id': 2,
+    //         "category_name": "nerd",
+    //         "scientific_name": "nerd?"
+    //     },
+    // ]
+    // const [data, setData] = useState([]);
 
-    useEffect(() => {
-        axios.get(axios.defaults.baseURL + "/tax/domain/")
-        .then((res) => {
-            console.log(res.data)
-            setData(res.data)
-        })
-    }, [])
-
+    // useEffect(() => {
+    //     axios.get(axios.defaults.baseURL + "/tax/domain/")
+    //     .then((res) => {
+    //         console.log(res.data)
+    //         setData(res.data)
+    //     })
+    // }, [])
+    console.log(data)
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+    })
     return(
         <div>
-            {
-                data.map((domain, domain_id) => {
-                    return(
-                        <h1>{domain.id}</h1>
-                    )
-                })
-            }
+            <table>
+                {table.getHeaderGroups().map(headerGroup => (
+                    <tr key={headerGroup.id}>
+                        {headerGroup.headers.map(header => 
+                            <th key={header.id}>
+                                {
+                                    flexRender(header.column.columnDef.header, header.getContext)
+                                }
+                            </th>
+                            )
+                        }
+                    </tr>
+                ))}
+                <tbody>
+                    {
+                        table.getRowModel().rows.map(row => (
+                            <tr key={row.id}>
+                                {row.getVisibleCells().map(cell => (
+                                    <td id={cell.id}>{
+                                        flexRender(cell.column.columnDef.cell, cell.getContext())
+                                    }</td>
+                                ))}
+                            </tr>
+                        ))
+                    }
+                </tbody>
+                <tfoot>
+                <button onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>{"<"}</button>
+                {table.getState().pagination.pageIndex + 1}
+                <button onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>{">"}</button>
+                </tfoot>
+            </table>
         </div>
     )
 }
