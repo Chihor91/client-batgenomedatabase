638bb2c76a76b6395302d3524b045403c81a8d81
ngarcia <ngarcia@up.edu.ph>
2023-08-10 15:31:18 +0800

    Refactor taxonomy pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import axios from 'axios'
 // Component Imports
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
-import Domain from './pages/Taxonomy/Domain/Domain'
 import NavBar from './components/NavBar/NavBar'
 import SideBar from './components/SideBar/SideBar'
 import { AuthProvider } from './context/AuthContext'
@@ -17,32 +16,25 @@ import viteLogo from '/vite.svg'
 
 // Style Imports
 import './App.css'
-import Phylum from './pages/Taxonomy/Phylum/Phylum'
-import Class from './pages/Taxonomy/Class/Class'
-import Order from './pages/Taxonomy/Order/Order'
-import Family from './pages/Taxonomy/Family/Family'
-import Genus from './pages/Taxonomy/Genus/Genus'
-import Species from './pages/Taxonomy/Species/Species'
+import Taxonomy from './pages/Taxonomy/Taxonomy'
 
 // Server API address
 axios.defaults.baseURL = "http://127.0.0.1:8000"
 // axios.defaults.baseURL = "http://202.92.144.124:8000/";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Root />}>
           <Route index element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/taxonomy/domain" element={<Domain />} />
-          <Route path="/taxonomy/phylum" element={<Phylum />} />
-          <Route path="/taxonomy/class" element={<Class />} />
-          <Route path="/taxonomy/order" element={<Order />} />
-          <Route path="/taxonomy/family" element={<Family />} />
-          <Route path="/taxonomy/genus" element={<Genus />} />
-          <Route path="/taxonomy/species" element={<Species />} />
+          <Route path="/taxonomy/domain" element={<Taxonomy category={"domain"} />} />
+          <Route path="/taxonomy/phylum" element={<Taxonomy category={"phylum"} />} />
+          <Route path="/taxonomy/class" element={<Taxonomy category={"class"} />} />
+          <Route path="/taxonomy/order" element={<Taxonomy category={"order"} />} />
+          <Route path="/taxonomy/family" element-={<Taxonomy category={"family"} />} />
+          <Route path="/taxonomy/genus" element={<Taxonomy category={"genus"} />} />
+          <Route path="/taxonomy/species" element={<Taxonomy category={"species"} />} />
         </Route>
       
     )
