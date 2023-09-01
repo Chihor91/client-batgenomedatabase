740ccb90026a5c80a45f58e82d01463b101c0f1d
ngarcia <ngarcia@up.edu.ph>
2023-09-01 12:28:57 +0800

    Implement private routes

---
+++ src/utils/AdminRoutes.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+
+const AdminRoutes = () =>  {
+    let navigate = useNavigate()
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        axios.get(axios.defaults.baseURL + "/user/isadmin/")
+        .then((res) => {
+            setLoading(false)
+        })
+        .catch((err) => {
+            navigate('/login')
+        })
+    })
+
+    return(
+        loading ? 
+        <h1>Loading</h1> :
+        <Outlet />
+    )
+}
+
+export default AdminRoutes
---
+++ src/utils/PrivateRoutes.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+
+const PrivateRoutes = () =>  {
+    let navigate = useNavigate()
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        axios.get(axios.defaults.baseURL + "/user/isloggedin/")
+        .then((res) => {
+            setLoading(false)
+        })
+        .catch((err) => {
+            navigate('/login')
+        })
+    })
+
+    return(
+        loading ? 
+        <h1>Loading</h1> :
+        <Outlet />
+    )
+}
+
+export default PrivateRoutes
