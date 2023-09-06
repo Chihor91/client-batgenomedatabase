4de81ca234d6a97fdabeb97d79df38e9edf13ddc
Chihor91 <ngarcia@up.edu.ph>
2023-09-06 15:23:47 +0800

    Revise style and implement dashboard and bookmarks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // Library Imports
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { createBrowserRouter, createRoutesFromElements, Route, Link, Outlet, RouterProvider } from 'react-router-dom'
 import axios from 'axios'
 
@@ -8,18 +8,16 @@ import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import NavBar from './components/NavBar/NavBar'
 import SideBar from './components/SideBar/SideBar'
-import { AuthProvider } from './context/AuthContext'
-import { ThemeProvider } from './components/ui/theme-provider'
-
-// Asset Imports
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-// Style Imports
-import './App.css'
+import Bookmarks from './pages/Bookmarks/Bookmarks'
 import Taxonomy from './pages/Taxonomy/Taxonomy'
 import TaxEntry from './pages/Taxonomy/TaxEntry'
+import Dashboard from './pages/Dashboard/Dashboard'
+import { AuthProvider } from './context/AuthContext'
+import { ThemeProvider } from './components/ui/theme-provider'
 import PrivateRoutes from './utils/PrivateRoutes'
+
+import './App.css'
+
 
 // Server API address
 axios.defaults.baseURL = "http://127.0.0.1:8000"
@@ -32,6 +30,8 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route element={<PrivateRoutes />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/bookmarks" element={<Bookmarks />} />
             <Route path="/domain" element={<Taxonomy rank={"domain"} />} />
             <Route path="/kingdom" element={<Taxonomy rank={"kingdom"} />} />
             <Route path="/phylum" element={<Taxonomy rank={"phylum"} />} />
@@ -49,7 +49,7 @@ function App() {
 
   return (
     <>
-      <RouterProvider router={router} />
+    <RouterProvider router={router} /> 
     </>
   )
 }
@@ -63,11 +63,15 @@ const Root = () => {
       <SideBar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
       <div className={`fixed transition-all ${
         showSidebar ? 
-        "left-[200px]" :
+        "left-[200px] w-[calc(100%-200px)]" :
         "left-0"
       }`}>
         <NavBar showSideBar={showSidebar} />
-        <div className="content">
+        <div className={`mt-[64px] h-full transition-all ${
+          showSidebar ?
+          "w-[calc(100vw-200px)]" :
+          "w-[100vw]"
+        }`} onClick={() => setShowSidebar(false)}>
           <Outlet />
         </div>
       </div>
