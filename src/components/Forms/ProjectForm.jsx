3db11d5aa0a89cb4a7180c9dabafa330bed8fb1e
ngarcia <ngarcia@up.edu.ph>
2023-09-25 04:07:52 +0800

    Refactor project form

---
+++ src/components/Forms/ProjectForm.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { Dialog, DialogActions, DialogTitle } from "@mui/material";
+import axios from "axios";
+import { Input } from "../ui/input";
+import { useForm } from "react-hook-form";
+
+
+function FormModal({open, handleClose}) {
+    const form = useForm({})
+
+    const { register, handleSubmit } = form
+
+    const onSubmit = (data) => {
+
+        axios.post("source/project/", data)
+        .then((res) => {
+            window.location.reload(false)
+        })
+    }
+
+    return (
+        <Dialog fullWidth={true} maxWidth="md" open={open} onClose={handleClose}>
+            <DialogTitle>Add New Project</DialogTitle>
+            <form onSubmit={handleSubmit(onSubmit)} className="m-10 space-y-3 w-auto text-gray-100">
+                <Input {...register("name")} type="text" placeholder="Project Name" />
+                <Input {...register("abbr")} type="text"  placeholder="Project Abbr." />
+                <DialogActions>
+                    <Button type="button" variant="outline" onClick={handleClose}>Cancel</Button>
+                    <Button type="submit" variant="outline">Add</Button>
+                </DialogActions>
+            </form>
+        </Dialog>
+    )
+}
+
+export default function ProjectForm() {
+    const [open, setOpen] = useState(false)
+
+    const handleOpen = () => {
+        setOpen(true)
+    }
+    
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    return(
+        <>
+            <Button variant="outline" onClick={handleOpen}>Add New Project</Button>
+            <FormModal open={open} handleClose={handleClose} />
+        </>
+    )
+}
