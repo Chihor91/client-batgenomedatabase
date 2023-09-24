0d5e836f5a03af02ef4d96ed9fcdcfcc99a4f297
ngarcia <ngarcia@up.edu.ph>
2023-09-25 04:07:38 +0800

    Remove tax routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // Library Imports
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { createBrowserRouter, createRoutesFromElements, Route, Link, Outlet, RouterProvider } from 'react-router-dom'
 import axios from 'axios'
 
@@ -7,20 +7,18 @@ import axios from 'axios'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Project from './pages/Project'
+import Source from './pages/Source'
 import NavBar from './components/NavBar/NavBar'
 import SideBar from './components/SideBar/SideBar'
 import { AuthProvider } from './context/AuthContext'
 import { ThemeProvider } from './components/ui/theme-provider'
 
 // Asset Imports
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 // Style Imports
 import './App.css'
-import Taxonomy from './pages/Taxonomy/Taxonomy'
-import TaxEntry from './pages/Taxonomy/TaxEntry'
 import PrivateRoutes from './utils/PrivateRoutes'
+import AdminRoutes from './utils/AdminRoutes'
 
 // Server API address
 axios.defaults.baseURL = "http://127.0.0.1:8000"
@@ -34,15 +32,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route element={<PrivateRoutes />}>
             <Route path="/project" element={<Project />} />
-            <Route path="/domain" element={<Taxonomy rank={"domain"} />} />
-            <Route path="/kingdom" element={<Taxonomy rank={"kingdom"} />} />
-            <Route path="/phylum" element={<Taxonomy rank={"phylum"} />} />
-            <Route path="/class" element={<Taxonomy rank={"class"} />} />
-            <Route path="/order" element={<Taxonomy rank={"order"} />} />
-            <Route path="/family" element={<Taxonomy rank={"family"} />} />
-            <Route path="/genus" element={<Taxonomy rank={"genus"} />} />
-            <Route path="/species" element={<Taxonomy rank={"species"} />} />
-            <Route path="/:rank/:id" element={<TaxEntry/>} />
+            <Route path="/source" element={<Source />} />
           </Route>
         </Route>
       
