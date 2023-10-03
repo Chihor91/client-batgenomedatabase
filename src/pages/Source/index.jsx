808fab5ce0fd14a1c4835a9b183e8b30459e483b
ngarcia <ngarcia@up.edu.ph>
2023-10-04 01:08:43 +0800

    Add navigation to source form

--- src/pages/Source/index.jsx
+++ src/pages/Source/index.jsx
@@ -4,9 +4,11 @@ import DataTable from "@/components/DataTable/ProjectTable";
 import { columns } from "./columns";
 import { Button } from "@/components/ui/button";
 import SourceForm from "@/components/Forms/SourceForm";
+import { useNavigate } from "react-router-dom";
 
 function Source() {
     const [data, setData] = useState([]);
+    let navigate = useNavigate()
 
     useEffect(() => {
         axios.get(axios.defaults.baseURL + "/source/source/")
@@ -18,7 +20,7 @@ function Source() {
     return(
         <>
             <div className="container mx-auto py-10 space-y-3">
-                <Button variant="outline">Add New Source</Button>
+                <Button variant="outline" onClick={() => navigate("/source/add")}>Add New Source</Button>
                 <DataTable data={data} columns={columns} />
             </div>
         </>
