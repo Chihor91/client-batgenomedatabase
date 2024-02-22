76889fc659aa664398d111bba3794d76458ad949
Reister <rei.navasero@gmail.com>
2024-02-22 15:22:35 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 // Library Imports
 import { useState } from 'react'
-import { createBrowserRouter, createRoutesFromElements, Route, Link, Outlet, RouterProvider } from 'react-router-dom'
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	Link,
+	Outlet,
+	RouterProvider,
+} from 'react-router-dom'
 import axios from 'axios'
 
 // Component Imports
@@ -26,62 +33,57 @@ import './App.css'
 import PrivateRoutes from '@/utils/PrivateRoutes'
 import AdminRoutes from '@/utils/AdminRoutes'
 
-
-
 // Server API address
-axios.defaults.baseURL = "http://127.0.0.1:8000"
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
 // axios.defaults.baseURL = "http://202.92.144.124:8000/";
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<Root />}>
-          <Route index element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route element={<PrivateRoutes />}>
-            <Route path="/project" element={<Project />} />
-            <Route path="/source" element={<Source />} />
-            <Route path="/source/add" element={<SourceForm />} />
-            <Route path="/isolate" element={<Isolate />} />
-            <Route path="/isolate/add" element={<StrainForm />} />
-          </Route>
-        </Route>
-      
-    )
-  )
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path='/' element={<Root />}>
+				<Route index element={<Home />} />
+				<Route path='/login' element={<Login />} />
+				<Route element={<PrivateRoutes />}>
+					<Route path='/project' element={<Project />} />
+					<Route path='/source' element={<Source />} />
+					<Route path='/source/add' element={<SourceForm />} />
+					<Route path='/isolate' element={<Isolate />} />
+					<Route path='/isolate/add' element={<StrainForm />} />
+				</Route>
+			</Route>,
+		),
+	)
 
-  return (
-    <>
-    <RouterProvider router={router} /> 
-    </>
-  )
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	)
 }
 
 const Root = () => {
-  const [showSidebar, setShowSidebar] = useState(false)
+	const [showSidebar, setShowSidebar] = useState(false)
 
-  return (
-    <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
-    <AuthProvider>
-      <SideBar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
-      <div className={`fixed transition-all ${
-        showSidebar ? 
-        "left-[200px] w-[calc(100%-200px)]" :
-        "left-0"
-      }`}>
-        <NavBar showSideBar={showSidebar} />
-        <body className={`w-[100%] overflow-y-scroll transition-all pt-[96px] h-[100vh] ${
-          showSidebar ?
-          "w-[calc(100vw-200px)]" :
-          "w-[100vw]"
-        }`} onClick={() => setShowSidebar(false)}>
-          <Outlet />
-        </body>
-      </div>
-    </AuthProvider>
-    </ThemeProvider>
-  )
+	return (
+		<ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+			<AuthProvider>
+				<SideBar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+				<div
+					className={`fixed transition-all ${
+						showSidebar ? 'left-[200px] w-[calc(100%-200px)]' : 'left-0'
+					}`}>
+					<NavBar showSideBar={showSidebar} />
+					<body
+						className={`w-[100%] overflow-y-scroll transition-all pt-[96px] h-[100vh] ${
+							showSidebar ? 'w-[calc(100vw-200px)]' : 'w-[100vw]'
+						}`}
+						onClick={() => setShowSidebar(false)}>
+						<Outlet />
+					</body>
+				</div>
+			</AuthProvider>
+		</ThemeProvider>
+	)
 }
-
 
 export default App
