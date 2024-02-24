ece33fc6f8647cc1b5b2f7bb541fd31b3a36ee4c
Reister <rei.navasero@gmail.com>
2024-02-24 12:22:58 +0800

    mx

--- src/components/Custom/CustomButton.jsx
+++ src/components/Custom/CustomButton.jsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
 				default: 'bg-primary text-primary-foreground hover:bg-primary/90',
 				destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
 				outline:
-					'border-input  bg-background hover:bg-background text-foreground hover:bg-background hover:shadow-lg hover:shadow-cyan-600/90 font-thin',
+					'border-input  bg-background hover:bg-background text-foreground hover:bg-background hover:shadow-lg hover:shadow-cyan-600/90 font-thin hover:font-black',
 				secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
 				ghost: 'hover:bg-accent hover:text-accent-foreground',
 				link: 'text-primary underline-offset-4 hover:underline',
