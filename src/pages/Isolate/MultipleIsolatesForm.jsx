c112d1fd9850f65b513d0b1c792c0c05bffbdc3d
ngarcia <ngarcia@up.edu.ph>
2024-03-16 22:01:32 +0800

    implement file upload add

--- src/pages/Isolate/MultipleIsolatesForm.jsx
+++ src/pages/Isolate/MultipleIsolatesForm.jsx
@@ -1,21 +1,25 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import axios from "axios"
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 
 export default function MultipleIsolateForm() {
     const form = useForm({})
+    const [results, setResults] = useState([])
     
     const onSubmit = (data) => {
         let form_data = new FormData()
         console.log(data.isolates[0])
         form_data.append("isolates", data.isolates[0])
-        axios.post(axios.defaults.baseURL + "/source/isolate/import_from_file/", form_data, {
+        axios.post(axios.defaults.baseURL + "/source/isolate/add/file/", form_data, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
+        }).then((res) => {
+            setResults(res.data.results)
+            console.log(res.data)
         })
-
     }
     return (
        <div>
@@ -23,7 +27,19 @@ export default function MultipleIsolateForm() {
                 <Input type="file" accept='.csv' {...form.register("isolates", { required: "This field is required" })} />
                 <Button type="submit" variant="outline">Upload File</Button>
             </form>
-            {JSON.stringify(form.watch(form))}
+                <ul>{results.length > 0 &&
+                    results.map((result, key) => 
+                        <li key={key} className="flex justify-center space-x-2">
+                            <div>
+                            {result.success ?
+                                "Success":
+                                "Fail"
+                            }
+                            </div>
+                            <div>{result.message}</div>
+                        </li>
+                    )
+                }</ul>
        </div>
     )
 }
