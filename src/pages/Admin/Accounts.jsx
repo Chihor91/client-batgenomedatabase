acb93d6c055fdce77aef1b9679d1b994fb9b4f29
ngarcia <ngarcia@up.edu.ph>
2024-03-17 23:51:59 +0800

    refactor account dialog

--- src/pages/Admin/Accounts.jsx
+++ src/pages/Admin/Accounts.jsx
@@ -1,10 +1,14 @@
 import {
-	TextField,
-	Paper,
 	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
 	DialogTitle,
-	DialogActions,
-} from '@mui/material'
+	DialogTrigger
+} from '@/components/ui/dialog'
+
+
 import {
 	flexRender,
 	getSortedRowModel,
@@ -26,10 +30,10 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { enqueueSnackbar, useSnackbar } from 'notistack'
+import { Input } from '@/components/ui/input'
 
-function AddAccount(open, handleClose) {
+function AddAccount(open, setOpen) {
 	const form = useForm({})
-
 	const { watch, register, handleSubmit, formState } = form
 	const { errors } = formState
 	const watchForm = watch(form)
@@ -42,104 +46,85 @@ function AddAccount(open, handleClose) {
 					variant: 'success',
 					autoHideDuration: 1000,
 				})
-				handleClose()
+				setOpen(false)
 			})
 			.catch((error) => {
 				enqueueSnackbar('Failed to create the account. Please try again.', {
 					variant: 'error',
 					autoHideDuration: 1000,
 				})
+				setOpen(false)
 				console.log(error)
 			})
 	}
 
 	return (
-		<Dialog
-			fullWidth={true}
-			PaperProps={{
-				style: { backgroundColor: '#007E10 ', borderRadius: '20px', borderWidth: '2px' },
-			}}
-			maxWidth='md'
-			open={open}
-			onClose={handleClose}>
-			<DialogTitle style={{ fontWeight: '800', fontSize: '1.5rem', color: 'white' }}>
-				Add New Account
-			</DialogTitle>
-
-			<form
-				onSubmit={handleSubmit(onSubmit)}
-				className='m-5 flex flex-col space-y-4 w-auto text-primary '>
-				<TextField
-					size='small'
-					{...register('username', { required: 'Please fill out this field' })}
-					id='username'
-					type='text'
-					name='username'
-					label='Username'
-					variant='filled'
-					error={errors.username}
-					helperText={errors.username?.message}
-				/>
-				<TextField
-					size='small'
-					{...register('email', {
-						required: 'Please fill out this field',
-						pattern: {
-							value: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/,
-							message: 'Invalid email format',
-						},
-					})}
-					type='text'
-					id='email'
-					autoComplete='off'
-					name='email'
-					label='Email Address'
-					variant='filled'
-					error={errors.email}
-					helperText={errors.email?.message}
-				/>
-				<TextField
-					size='small'
-					{...register('password', { required: 'Please fill out this field' })}
-					type='password'
-					id='password'
-					name='password'
-					variant='filled'
-					label='Password'
-					error={errors.password}
-					helperText={errors.password?.message}
-				/>
-				<TextField
-					size='small'
-					{...register('re_password', {
-						validate: (fieldValue) => {
-							return fieldValue === watchForm.password || 'Passwords do not match'
-						},
-					})}
-					type='password'
-					variant='filled'
-					label='Confirm Password'
-					error={errors.re_password}
-					helperText={errors.re_password?.message}
-				/>
-				<DialogActions>
-					<Button type='button' variant='ghost' onClick={handleClose}>
-						Cancel
-					</Button>
-					<Button type='submit' variant='ghost'>
-						Add
-					</Button>
-				</DialogActions>
-			</form>
+		<Dialog open={open} onOpenChange={setOpen}>
+			<DialogTrigger asChild>
+				<Button variant="outline" onClick={() => setOpen(true)}>Add Account</Button>
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader className="text-xl font-semibold">
+					Add New Account
+				</DialogHeader>
+				<form
+					onSubmit={handleSubmit(onSubmit)}
+					className='m-5 flex flex-col space-y-4 w-auto'>
+					<Input
+						{...register('username', { required: 'Please fill out this field' })}
+						type='text'
+						placeholder='Username'
+						error={errors.username}
+						helperText={errors.username?.message}
+					/>
+					<Input
+						size='small'
+						{...register('email', {
+							required: 'Please fill out this field',
+							pattern: {
+								value: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/,
+								message: 'Invalid email format',
+							},
+						})}
+						type='text'
+						placeholder='Email Address'
+						error={errors.email}
+						helperText={errors.email?.message}
+					/>
+					<Input
+						{...register('password', { required: 'Please fill out this field' })}
+						type='password'
+						placeholder='Password'
+						error={errors.password}
+						helperText={errors.password?.message}
+					/>
+					<Input
+						{...register('re_password', {
+							validate: (fieldValue) => {
+								return fieldValue === watchForm.password || 'Passwords do not match'
+							},
+						})}
+						type='password'
+						placeholder='Repeat Password'
+						error={errors.re_password}
+						helperText={errors.re_password?.message}
+					/>
+					<DialogFooter>
+						<Button type='submit' variant='outline'>
+							Add
+						</Button>
+					</DialogFooter>
+				</form>
+			</DialogContent>
+			
 		</Dialog>
 	)
 }
 
 export default function Accounts() {
 	const [data, setData] = useState([])
-	const [open, setOpen] = useState(false)
 	const [sorting, setSorting] = useState([])
-
+	const [open, setOpen] = useState(false)
 	const table = useReactTable({
 		data,
 		columns,
@@ -163,9 +148,6 @@ export default function Accounts() {
 		<div className='space-y-2'>
 			<h1 className='text-center text-lg w-full font-semibold'>Accounts</h1>
 			<div className='rounded-md border'>
-				{AddAccount(open, () => {
-					setOpen(false)
-				})}
 				<Table>
 					<TableHeader>
 						{table.getHeaderGroups().map((headerGroup) => (
@@ -209,9 +191,7 @@ export default function Accounts() {
 					</TableBody>
 				</Table>
 			</div>
-			<Button variant='outline' onClick={() => setOpen(true)}>
-				Add Account
-			</Button>
+			{AddAccount(open, setOpen)}
 		</div>
 	)
 }
